6e194b4f0426f591eef4db142ee6c0fcdee3628e
Youssef samuel <134071262+youssefsamue@users.noreply.github.com>
2024-12-27 20:25:53 +0200

    Delete SummaryService directory

--- SummaryService/app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import summarize, get_status
-
-urlpatterns = [
-    path("summarize", summarize, name="summarize"),
-    path("summarize/status/<str:id>", get_status, name="get_status"),
-]
